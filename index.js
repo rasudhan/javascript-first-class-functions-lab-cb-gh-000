ebe57b0acc122ed50caf47b00de87a84e9e80a90
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 14:53:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 function countdown(callback) {
-  window.setTimeout(2);
-  callback();
+  setTimeout(function () {
+    callback();
+  }, 2000);
 }
 
 const countdown = (cb) => {
   window.setTimeout(cb, 2000)
 }
 
+function createMultiplier(multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
+}
  // 2
 const createMultiplier = (multi) => {
     return function multiplyNumber(number) {
-      return multi * val
+      return multi * number
     }
 }
  const doubler = createMultiplier(2)
@@ -23,3 +29,13 @@ const multiplier = (multiplierValue, value) => {
 }
  const doublerWithBind = multiplier.bind(null, 2)
 const triplerWithBind = multiplier.bind(null, 3)
+
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
