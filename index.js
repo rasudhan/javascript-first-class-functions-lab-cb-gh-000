37cf712280582a0e3b24bfb0497e1246cc81c808
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 14:53:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,13 @@ const multiplier = (multiplierValue, value) => {
 }
  const doublerWithBind = multiplier.bind(null, 2)
 const triplerWithBind = multiplier.bind(null, 3)
+
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
