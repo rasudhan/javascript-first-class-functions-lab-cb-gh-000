023e77c6c03b193ca5793220fe1650a83b1635ee
Anandha Sudhan <rasudhan@gmail.com>
2018-10-08 14:51:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function countdown(callback) {
+  window.setTimeout(2);
+  callback();
+}
+
+const countdown = (cb) => {
+  window.setTimeout(cb, 2000)
+}
+
+ // 2
+const createMultiplier = (multi) => {
+    return function multiplyNumber(number) {
+      return multi * val
+    }
+}
+ const doubler = createMultiplier(2)
+const tripler = createMultiplier(3)
+ // 3
+const multiplier = (multiplierValue, value) => {
+  return function multiplyNumber(multiplierValue, value) {
+    return multiplierValue * value
+  }
+}
+ const doublerWithBind = multiplier.bind(null, 2)
+const triplerWithBind = multiplier.bind(null, 3)
